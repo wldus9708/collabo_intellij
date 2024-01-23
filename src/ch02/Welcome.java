d7a59a0f2e47201d20485ecc8e7534c90a509f93
butterfly <butterfly@anver.com>
2024-01-23 12:49:38 +0900

    pull 하지않은 데이터를 push하려고시도합니다.

--- src/ch02/Welcome.java
+++ src/ch02/Welcome.java
@@ -2,6 +2,6 @@ package ch02;
     public class Welcome {
         public static void main(String[] args) {
             System.out.println("welcome01");
-            System.out.println("welcome02");
+            System.out.println("welcome02_written_by_hee");
         }
     }
