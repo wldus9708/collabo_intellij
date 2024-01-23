42fe16b038a613a1fccc61f9ee65d98e593a27cd
butterfly <butterfly@anver.com>
2024-01-23 11:52:24 +0900

    두개의 파일이 수정되었습니다.

--- src/ch01/Hello.java
+++ src/ch01/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello 01");
         System.out.println("hello 02");
+        System.out.println("hello 03");
     }
 }
--- src/ch01/World.java
+++ src/ch01/World.java
@@ -3,5 +3,6 @@ package ch01;
 public class World {
     public static void main(String[] args) {
         System.out.println("world 01");
+        System.out.println("world 02");
     }
 }
