0e92c821861a4d6f4f2a49904b1dca2a68221e63
butterfly <butterfly@anver.com>
2024-01-23 12:55:19 +0900

    최종적으로 푸시합니다.

--- src/ch02/Welcome.java
+++ src/ch02/Welcome.java
@@ -3,5 +3,6 @@ package ch02;
         public static void main(String[] args) {
             System.out.println("welcome01");
             System.out.println("welcome02_written_by_hee");
+            System.out.println("welcome02");
         }
     }
